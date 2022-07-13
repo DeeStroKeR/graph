ae064a7742e2473a3db2b7e156108dcfa92f2baf
MRuletkin <mruletkin6@gmail.com>
2022-07-13 11:40:37 +0300

    mn commit

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code
-
-
 def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
